9e9e051a976341e03f01bac668d89858cf56d733
Joaquin Terzano <128100984+JoaquinTerzano@users.noreply.github.com>
2025-12-04 17:25:12 -0300

    Remove socketio initialization from app.py
    Removed socketio initialization from the main section.

--- app.py
+++ app.py
@@ -121,10 +121,5 @@ def handle_options():
 def health_check():
     return "Ok"
 
-
-# MAIN
-# Inicializas socketio con la app
-socketio.init_app(app)
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
