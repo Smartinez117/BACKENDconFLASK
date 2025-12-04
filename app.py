24b9a12f7bb00e97df6d5c65a292df82c99eb126
Joaquin Terzano <128100984+JoaquinTerzano@users.noreply.github.com>
2025-12-04 17:07:01 -0300

    Refactor Firebase configuration to use environment variables
    Removed hardcoded Firebase configuration and replaced it with environment variable loading for credentials.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ if not hasattr(builtins, "unicode"):
     builtins.unicode = str
 
 import os
+import json
 import psycopg2
 from firebase_admin import credentials, auth as firebase_auth
 from flask import Flask, request, jsonify
@@ -52,21 +53,6 @@ def cerrar_sesion():
 def shutdown_session(exception=None):
     """Cierra la sesión de base de datos al finalizar el contexto de la app."""
     cerrar_sesion()
-
-# Configuración Firebase
-service_account_info = {
-    "type": os.environ.get("FIREBASE_TYPE"),
-    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": str(os.environ.get("FIREBASE_PRIVATE_KEY")).replace('\\n', '\n'),
-    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
-    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
-    "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
-}
 
 # Inicializar Firebase
 # cred = credentials.Certificate("firebase/firebase-credentials.json")
